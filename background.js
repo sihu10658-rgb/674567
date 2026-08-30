4d18730eef509a2318053edcc6a0bf766505fc69
sihu10658-rgb <sihu10658@gmail.com>
2026-08-30 14:22:20 +0900

    Remove redundant comments and clean up code

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
+// 광고 차단 서비스 워커 (Manifest V3)
 
-// 광고 차단 서비스 워커 (Manifest V3 호환)
-
-// 확장프로그램 설치 시 기본 설정 초기화
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Ad Blocker 확장프로그램이 설치되었습니다.');
   chrome.storage.local.set({
@@ -11,7 +9,6 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-// 콘텐츠 스크립트나 팝업에서 보내는 메시지 처리 (광고 차단 카운트 누적 등)
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'blockAd') {
     chrome.storage.local.get(['blockedCount'], (result) => {
