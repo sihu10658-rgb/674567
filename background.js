20bafa2c3441015e570f14e9f2ee9ee52d7a58bc
sihu10658-rgb <sihu10658@gmail.com>
2026-08-30 14:41:43 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,24 +1,10 @@
-// 광고 차단 서비스 워커 (Manifest V3)
+let blockedCount = 0;
 
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Ad Blocker 확장프로그램이 설치되었습니다.');
-  chrome.storage.local.set({
-    enabled: true,
-    blockedCount: 0,
-    whitelist: []
-  });
+chrome.declarativeNetRequest.onRuleMatchedDebug.forEach?.(() => {
+  blockedCount++;
+  chrome.storage.local.set({ blockedCount });
 });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'blockAd') {
-    chrome.storage.local.get(['blockedCount'], (result) => {
-      chrome.storage.local.set({
-        blockedCount: (result.blockedCount || 0) + 1
-      });
-    });
-  } else if (request.action === 'getStats') {
-    chrome.storage.local.get(['blockedCount'], (result) => {
-      sendResponse({ blockedCount: result.blockedCount || 0 });
-    });
-  }
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({ blockedCount: 0 });
 });
